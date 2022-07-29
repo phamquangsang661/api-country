7d7043a97dd9e6a851c1672f59b807474d496856
Phạm Quang Sáng <phamquangsang661@gmail.com>
2022-07-29 19:01:13 +0700

    api

---
+++ index.js
@@ -0,0 +1,21 @@
+const lib = require('country-state-city')
+let Country = require('country-state-city').Country;
+
+
+Country.getAllCountries()
+console.log(lib.City.getCitiesOfCountry('VN'))
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+app.get('/country/cities', (req, res) => {
+    const { country_code } = req.query
+    return res.json(lib.City.getCitiesOfCountry(country_code))
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
